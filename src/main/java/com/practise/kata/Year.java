e638cec2618734149d7a8472225c181dd069e063
Varunesh Ram ThangaPerumal <varuneshram@yahoo.com>
2020-01-08 00:05:36 +0530

    Added 2019 year should return not leap year

--- src/main/java/com/practise/kata/Year.java
+++ src/main/java/com/practise/kata/Year.java
@@ -1,4 +1,7 @@
 package com.practise.kata;
 
 public class Year {
+    public boolean isLeapYear(int year) {
+        return false;
+    }
 }
--- src/test/java/com/practise/kata/YearTest.java
+++ src/test/java/com/practise/kata/YearTest.java
@@ -5,8 +5,14 @@ import org.junit.Test;
 
 public class YearTest {
     @Test
-    public void shouldInstantiateYear(){
+    public void shouldInstantiateYear() {
         Year year = new Year();
         Assert.assertNotNull(year);
+    }
+
+    @Test
+    public void year2019ShouldNotBeLeapYear() {
+        Year year = new Year();
+        Assert.assertFalse(year.isLeapYear(2019));
     }
 }
